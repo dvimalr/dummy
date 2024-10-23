722434703e7988dc0ad93986ae5f6c8724d60670
dvimalr <81092626+dvimalr@users.noreply.github.com>
2024-10-23 12:40:39 +0530

    Update my_procedure.sql

--- my_procedure.sql
+++ my_procedure.sql
@@ -1,19 +1,19 @@
+-- my_procedure.sql
+
 CREATE OR REPLACE PROCEDURE merge_tables()
 LANGUAGE plpgsql AS $$
 BEGIN
-    -- Drop the merged_table if it already exists
-    DROP TABLE IF EXISTS merged_table;
-
-    -- Create the merged_table
-    CREATE TABLE merged_table AS
+    -- Create a new table to store the merged results
+    CREATE TABLE IF NOT EXISTS merged_table AS
     SELECT 
         t1.id AS table1_id,
-        t1.name,
+        t1.name AS item_name,
+        t2.id AS table2_id,
         t2.description
     FROM 
         table1 t1
-    JOIN 
-        table2 t2 ON t1.id = t2.table1_id;
+    LEFT JOIN 
+        table2 t2 ON t1.id = t2.table1_id; -- Assuming table2 has a foreign key reference to table1
 
     RAISE NOTICE 'Tables merged successfully!';
 END;
